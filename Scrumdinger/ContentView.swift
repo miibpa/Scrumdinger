e604d5d3b6b5017369856a4da105d1a88f313747
Miguel Ibañez Paricio <mibanepa@mercadona.es>
2020-12-18 16:57:11 +0100

    Initial Commit

---
+++ Scrumdinger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Miguel Ibañez Patricio on 18/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Miguel Ibañez Patricio on 18/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
